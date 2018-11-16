6471ef04801ae40447bf75cf07a3de6e4b8ee6a4
Patrick Martin <patrickmartin88@gmail.com>
2018-11-16 10:34:20 -0800

    creating layout for list.

--- app/src/main/java/com/lambdaschool/sprint2_challenge/MainActivity.java
+++ app/src/main/java/com/lambdaschool/sprint2_challenge/MainActivity.java
@@ -2,11 +2,15 @@ package com.lambdaschool.sprint2_challenge;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.speech.RecognitionListener;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,19 +18,39 @@ public class MainActivity extends AppCompatActivity {
     private Activity activity;
     private RecyclerView recyclerView;
     private RecyclerView.LayoutManager layoutManager;
+    private GroceryListAdapter listAdapter;
+
+    private ArrayList<Grocery> groceryList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        context = this;
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        activity = this;
+
+
+        findViewById(R.id.select_item_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
         recyclerView = findViewById(R.id.shopping_list_view);
         recyclerView.setHasFixedSize(true);
-
         layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
 
-        String hey = ShoppingItemConstants.ITEM_NAMES_RAW[1];
+        for (int i = 0; i < ShoppingItemConstants.ICON_IDS.length; i++){
+            groceryList.add(new Grocery(ShoppingItemConstants.ICON_IDS[i], ShoppingItemConstants.ITEM_NAMES_RAW[i]));
+        }
+
+        listAdapter = new GroceryListAdapter(groceryList, activity);
+        recyclerView.setAdapter(listAdapter);
+
 
 
     }
