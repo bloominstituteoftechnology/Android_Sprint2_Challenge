aaf8e967ca172a94daee13544f77836679b5f878
Patrick Martin <patrickmartin88@gmail.com>
2018-11-16 08:38:35 -0800

    creating layout for list.

--- app/src/main/java/com/lambdaschool/sprint2_challenge/MainActivity.java
+++ app/src/main/java/com/lambdaschool/sprint2_challenge/MainActivity.java
@@ -1,9 +1,14 @@
 package com.lambdaschool.sprint2_challenge;
 
+import android.app.Activity;
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Context context;
+    private Activity activity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
