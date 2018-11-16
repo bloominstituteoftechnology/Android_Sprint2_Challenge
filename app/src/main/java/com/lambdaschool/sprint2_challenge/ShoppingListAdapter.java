a716514a7e04accf353861d941667cbe7bb21eb8
Jacob Seppanen <jbseppanen@gmail.com>
2018-11-16 08:36:54 -0800

    Initial Commit with recycler view layout.

---
+++ app/src/main/java/com/lambdaschool/sprint2_challenge/ShoppingListAdapter.java
@@ -0,0 +1,30 @@
+package com.lambdaschool.sprint2_challenge;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class ShoppingListAdapter  extends RecyclerView.Adapter<ShoppingListAdapter.ViewHolder>{
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+        LinearLayout linearLayout;
+        ImageView imageView;
+        TextView textView;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            linearLayout = itemView.findViewById(R.id.layout_items);
+            imageView = itemView.findViewById(R.id.image_item);
+            textView = itemView.findViewById(R.id.text_item);
+        }
+    }
+}
