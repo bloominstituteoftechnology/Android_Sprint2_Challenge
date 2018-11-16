6af54504eff67f79502ee256711235d22d797417
Patrick Martin <patrickmartin88@gmail.com>
2018-11-16 08:46:07 -0800

    creating layout for list.

--- app/src/main/java/com/lambdaschool/sprint2_challenge/MainActivity.java
+++ app/src/main/java/com/lambdaschool/sprint2_challenge/MainActivity.java
@@ -2,17 +2,22 @@ package com.lambdaschool.sprint2_challenge;
 
 import android.app.Activity;
 import android.content.Context;
+import android.speech.RecognitionListener;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
 
     private Context context;
     private Activity activity;
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.)
     }
 }
