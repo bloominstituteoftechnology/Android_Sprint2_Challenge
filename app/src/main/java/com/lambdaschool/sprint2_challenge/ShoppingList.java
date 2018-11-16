baa1853be1ec314b2612c4033f13ca4c6c989cf4
JTyzz <jtyzzer@gmail.com>
2018-11-16 13:59:05 -0800

    Jason Tyzzer Android Sprint Challenge 2

--- app/src/main/java/com/lambdaschool/sprint2_challenge/ShoppingList.java
+++ app/src/main/java/com/lambdaschool/sprint2_challenge/ShoppingList.java
@@ -2,7 +2,16 @@ package com.lambdaschool.sprint2_challenge;
 
 
 
-
 public class ShoppingList{
+    private String grocery_name;
+    private int grocery_icon;
+    private int id;
+
+    public ShoppingList(int id, String grocery_name, int grocery_icon){
+        this.id = id;
+        this.grocery_name = grocery_name;
+        this.grocery_icon = grocery_icon;
+    }
+
 
 }
