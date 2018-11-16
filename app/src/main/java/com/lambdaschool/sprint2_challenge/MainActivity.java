ea97f3f5fe22768d66ecf416fe1af613f6bd066e
ThaDocIzN <rodan72@msn.com>
2018-11-16 10:45:24 -0600

    added shopping item class

--- app/src/main/java/com/lambdaschool/sprint2_challenge/MainActivity.java
+++ app/src/main/java/com/lambdaschool/sprint2_challenge/MainActivity.java
@@ -3,6 +3,8 @@ package com.lambdaschool.sprint2_challenge;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
---
+++ app/src/main/java/com/lambdaschool/sprint2_challenge/ShoppingItem.java
@@ -0,0 +1,37 @@
+package com.lambdaschool.sprint2_challenge;
+
+public class ShoppingItem {
+    private String name;
+    private int image;
+    private int id;
+
+    public ShoppingItem(String name, int image, int id) {
+        this.name = name;
+        this.image = image;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
