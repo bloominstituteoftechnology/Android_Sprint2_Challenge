f6efa5ceabb58d33873febd34bae508aa3c1fbaf
BL-CoderOvertime <brandonlively@live.com>
2019-03-29 11:18:52 -0400

    Setup Item model class

--- app/src/main/java/com/lambdaschool/sprint2_challenge/Item.java
+++ app/src/main/java/com/lambdaschool/sprint2_challenge/Item.java
@@ -2,4 +2,40 @@ package com.lambdaschool.sprint2_challenge;
 
 public class Item {
 
+    String name;
+    int imageID;
+    boolean isSelected;
+    int id;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getImageID() {
+        return imageID;
+    }
+
+    public void setImageID(int imageID) {
+        this.imageID = imageID;
+    }
+
+    public boolean isSelected() {
+        return isSelected;
+    }
+
+    public void setSelected(boolean selected) {
+        isSelected = selected;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
