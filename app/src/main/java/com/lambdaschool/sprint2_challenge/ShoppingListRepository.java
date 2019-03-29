226e3f17b16354c7951040deaf77ff4b4a7f7ed1
VivekV95 <vivekv210@gmail.com>
2019-03-29 10:23:44 -0500

    Created repo and added method for populating arraylist of all shopping items

---
+++ app/src/main/java/com/lambdaschool/sprint2_challenge/ShoppingListRepository.java
@@ -0,0 +1,20 @@
+package com.lambdaschool.sprint2_challenge;
+
+import java.util.ArrayList;
+
+public class ShoppingListRepository {
+
+    private static ArrayList<ShoppingItem> shoppingItems;
+
+    private static ArrayList<ShoppingItem>  getShoppingItems() {
+        for (int i = 0; i < ShoppingItemConstants.ITEM_NAMES_RAW.length - 1; i++) {
+            ShoppingItem item = new ShoppingItem(
+                    ShoppingItemConstants.ITEM_NAMES_RAW[i]
+                    ,i , ShoppingItemConstants.ICON_IDS[i]);
+            shoppingItems.add(item);
+        }
+        return shoppingItems;
+    }
+
+    
+}
