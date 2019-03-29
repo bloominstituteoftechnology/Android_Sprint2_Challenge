f8ce65ffdc56d15faa17e0c4c22a28d14f418958
VivekV95 <vivekv210@gmail.com>
2019-03-29 14:24:20 -0500

    Added notification when list is sent and changed app theme

--- app/src/main/java/com/lambdaschool/sprint2_challenge/MainActivity.java
+++ app/src/main/java/com/lambdaschool/sprint2_challenge/MainActivity.java
@@ -1,20 +1,31 @@
 package com.lambdaschool.sprint2_challenge;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Build;
+import android.support.annotation.Nullable;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     RecyclerView recyclerView;
     LinearLayoutManager layoutManager;
     ShoppingListAdapter shoppingListAdapter;
+    Button sendButton;
     Context context;
     static SharedPreferences preferences;
+
+    static final int SHOPPING_NOTIFICATION_ID = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,17 +44,44 @@ public class MainActivity extends AppCompatActivity {
         shoppingListAdapter = new ShoppingListAdapter(ShoppingListRepository.getShoppingItems());
         recyclerView.setAdapter(shoppingListAdapter);
 
-        System.out.println(ShoppingListRepository.createShoppingList());
+        sendButton = findViewById(R.id.send_button);
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendList();
+            }
+        });
     }
 
     public void sendList() {
+
+        final NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "SL Channel";
+            String description = "This channel is used to share shopping lists";
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+            NotificationChannel channel = new NotificationChannel(getPackageName(), name, importance);
+            channel.setDescription(description);
+
+            notificationManager.createNotificationChannel(channel);
+        }
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, getPackageName())
+                .setPriority(NotificationManager.IMPORTANCE_HIGH)
+                .setContentTitle("Shopping")
+                .setContentText("Shopping List Sent!")
+                .setColor(getResources().getColor(R.color.colorPrimary))
+                .setSmallIcon(R.drawable.notification_icon)
+                .setDefaults(Notification.DEFAULT_ALL);
+
+        notificationManager.notify(MainActivity.SHOPPING_NOTIFICATION_ID, builder.build());
+
         Intent intent = new Intent();
         intent.setAction(Intent.ACTION_SEND);
         String shoppingList = ShoppingListRepository.createShoppingList();
         intent.setType("text/plain");
         intent.putExtra(Intent.EXTRA_TEXT, shoppingList);
-        startActivityForResult(intent, 1);
+        startActivity(intent);
+
     }
-
-
 }
