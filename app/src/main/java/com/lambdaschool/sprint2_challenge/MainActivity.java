129f9702248602e18b8b9bbf6619aaaacb5e6797
Spencer Stock <spencerstock@gmail.com>
2019-03-29 09:07:21 -0600

    initial commit

--- app/src/main/java/com/lambdaschool/sprint2_challenge/MainActivity.java
+++ app/src/main/java/com/lambdaschool/sprint2_challenge/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    // Checking VERSION CONTROL
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
