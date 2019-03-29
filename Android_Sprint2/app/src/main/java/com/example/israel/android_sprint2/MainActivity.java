e68e895df90fcf67e1eeb1daff67f4f49f32f4b1
Israel dela Cruz <igdcruz42@gmail.com>
2019-03-29 10:08:54 -0500

    added new project Android_Sprint2

--- Android_Sprint2/app/src/main/java/com/example/israel/android_sprint2/MainActivity.java
+++ Android_Sprint2/app/src/main/java/com/example/israel/android_sprint2/MainActivity.java
@@ -1,4 +1,4 @@
-package com.lambdaschool.sprint2_challenge;
+package com.example.israel.android_sprint2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,7 +9,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
     }
 }
