7066cf8ac9aacb38d1a17b8306e769f23bfd64bf
voodoopdx <ourquotesdefineus@gmail.com>
2019-04-05 11:29:46 -0700

    Item class created and filled, shared prefs added; will be corrected later

---
+++ app/src/main/java/com/lambdaschool/sprint2_challenge/ShoppingList.java
@@ -0,0 +1,13 @@
+package com.lambdaschool.sprint2_challenge;
+
+import java.util.ArrayList;
+
+public class ShoppingList {
+    public ShoppingList() {
+    }
+
+    public void add(ShoppingItem) {
+
+    }
+    public ArrayList<ShoppingItem> items= new ArrayList<ShoppingItem>();
+}
