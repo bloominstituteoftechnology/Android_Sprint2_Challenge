0665d570ef7c8217d93d87a116a099bfea6a9f6c
Albert Yakubov <mrayakubov@hotmail.com>
2019-08-16 12:40:53 -0600

    Shopping List
    add Notification

--- app/src/main/java/com/lambdaschool/sprint2_challenge/MainActivity.kt
+++ app/src/main/java/com/lambdaschool/sprint2_challenge/MainActivity.kt
@@ -1,17 +1,37 @@
 package com.lambdaschool.sprint2_challenge
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.content.ContextCompat
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.shopping_list_item.*
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        const val NOTIFICATION_ID = 1
+
+        const val CONSTANT_STRING = "CLICKS"
+        const val CONSTANT_STRING2 = "ACTION BAR TEXT"
+
+
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button_add.setOnClickListener {
+
+            //TODO call the notification heer
+            BasicNotification.BasicNotification(this)
+
+
+        }
+
+
 
         val data = mutableListOf<ShoppingList>()
         val almond = ContextCompat.getDrawable(this, R.drawable.almond)
@@ -26,5 +46,9 @@ class MainActivity : AppCompatActivity() {
         recycle.layoutManager = manager
         recycle.adapter = adapter
 
+
+
+
     }
+
 }
