a9efcec3f26fd5595c2a13e29c068585e8a56b82
Albert Yakubov <mrayakubov@hotmail.com>
2019-08-16 14:05:34 -0600

    Shopping List
    update images

--- app/src/main/java/com/lambdaschool/sprint2_challenge/MainActivity.kt
+++ app/src/main/java/com/lambdaschool/sprint2_challenge/MainActivity.kt
@@ -6,6 +6,8 @@ import android.os.Bundle
 import android.support.v4.content.ContextCompat
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.widget.CheckBox
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.shopping_list_item.*
 
@@ -25,30 +27,79 @@ class MainActivity : AppCompatActivity() {
 
         button_add.setOnClickListener {
 
+
             //TODO call the notification heer
             BasicNotification.BasicNotification(this)
 
-
         }
 
 
 
-        val data = mutableListOf<ShoppingList>()
-        val almond = ContextCompat.getDrawable(this, R.drawable.almond)
 
-        if (almond != null){
-            data.add(ShoppingList("Almond", almond))
+            val data = mutableListOf<ShoppingList>()
+
+
+
+            val almond = ContextCompat.getDrawable(this, R.drawable.almond)
+                if (almond != null) {
+                    data.add(ShoppingList("Almond", almond))
+            }
+        val greenApple = ContextCompat.getDrawable(this, R.drawable.apple_green)
+                if(greenApple != null){
+                    data.add(ShoppingList("Green Apple", greenApple))
+                }
+        val avocado = ContextCompat.getDrawable(this, R.drawable.avocado)
+        if (avocado != null) {
+            data.add(ShoppingList("Avocado", avocado))
         }
+        val bacon = ContextCompat.getDrawable(this, R.drawable.bacon)
+        if(bacon != null){
+            data.add(ShoppingList("Bacon", bacon))
+        }
+        val banana = ContextCompat.getDrawable(this, R.drawable.banana)
+        if (banana != null) {
+            data.add(ShoppingList("Banana", banana))
+        }
+        val barley = ContextCompat.getDrawable(this, R.drawable.barley)
+        if(barley != null){
+            data.add(ShoppingList("Barley", barley))
+        }
+        val beef = ContextCompat.getDrawable(this, R.drawable.beef)
+        if (beef != null) {
+            data.add(ShoppingList("Beef", beef))
+        }
+        val birthdayCake = ContextCompat.getDrawable(this, R.drawable.birthday_cake)
+        if(birthdayCake != null){
+            data.add(ShoppingList("Birthday Cake", birthdayCake))
+        }
+        val broccoli = ContextCompat.getDrawable(this, R.drawable.broccoli)
+        if (broccoli != null) {
+            data.add(ShoppingList("Broccoli", broccoli))
+        }
+        val candy = ContextCompat.getDrawable(this, R.drawable.candy)
+        if(candy != null){
+            data.add(ShoppingList("Candy", candy))
+        }
+        val cherry = ContextCompat.getDrawable(this, R.drawable.cherry)
+        if (cherry != null) {
+            data.add(ShoppingList("Cherry", cherry))
+        }
+        val chicken = ContextCompat.getDrawable(this, R.drawable.chicken)
+        if(chicken != null){
+            data.add(ShoppingList("Chicken", chicken))
+        }
+
+
+
+
 
         //populate almond
-        val manager = LinearLayoutManager(this, RecyclerView.VERTICAL, false)
-        val adapter = ShoppingListAdapter(data)
-        recycle.layoutManager = manager
-        recycle.adapter = adapter
+            val manager = LinearLayoutManager(this, RecyclerView.VERTICAL, false)
+            val adapter = ShoppingListAdapter(data)
+            recycle.layoutManager = manager
+            recycle.adapter = adapter
 
 
-
-
+        }
     }
 
-}
--- app/src/main/java/com/lambdaschool/sprint2_challenge/ShoppingList.kt
+++ app/src/main/java/com/lambdaschool/sprint2_challenge/ShoppingList.kt
@@ -1,7 +1,9 @@
 package com.lambdaschool.sprint2_challenge
 
 import android.graphics.drawable.Drawable
+import android.widget.CheckBox
 import java.io.Serializable
 
 data class ShoppingList(var name: String = "",
-                        var image: Drawable): Serializable
+                        var image: Drawable,
+                        var checkBox:  Boolean = false): Serializable
