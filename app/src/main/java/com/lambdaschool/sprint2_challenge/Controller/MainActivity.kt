1bb0bf207d1be10058f2cb8074263c57fdb43b98
jDanielDetes <jdanieldetes@gmail.com>
2019-08-16 11:20:59 -0400

    files organized into packages

---
+++ app/src/main/java/com/lambdaschool/sprint2_challenge/Controller/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.lambdaschool.sprint2_challenge.Controller
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.lambdaschool.sprint2_challenge.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
